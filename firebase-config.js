6b59bb420f4c4ebf87ca8073a930a5e763fcec16
cineopsy <cineopsy@gmail.com>
2026-03-13 23:50:26 +0530

    Initial Cineopsy Files

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// CinéOpsy — Firebase Config
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAM-IagvT3o7euvf4fklWMvvEvHblzKApM",
+  authDomain: "cineopsy-ade83.firebaseapp.com",
+  projectId: "cineopsy-ade83",
+  storageBucket: "cineopsy-ade83.firebasestorage.app",
+  messagingSenderId: "226892096791",
+  appId: "1:226892096791:web:68d018db38baa46e50c352",
+  measurementId: "G-CBLTSGGPYB"
+};
+
+// Admin Password
+const ADMIN_PASSWORD = "Cineopsy420@";
